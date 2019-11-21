a9b57b726463b796ad6d31c23cac5affda44f6be
JoshPhronesis <akosajoshua@yahoo.com>
2019-11-21 15:14:18 +0100

    Add project files.

---
+++ ReLINQ.UnitTests/ReLinqTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ReLINQ.UnitTests
+{
+    public class ReLinqTests
+    {
+        [Test]
+        public void Where_NullSourceThrowsNullArgumentException()
+        {
+            IEnumerable<int> source = null;
+            Assert.Throws<ArgumentNullException>(() => source.Where(x => x > 5));
+        }
+        [Test]
+        public void Where_NullPredicateThrowsNullArgumentException()
+        {
+            int[] source = { 1, 3, 7, 9, 10 };
+            Func<int, bool> predicate = null;
+            Assert.Throws<ArgumentNullException>(() => source.Where(predicate));
+        }
+        [Test]
+        public void Where_SimpleFiltering()
+        {
+            int[] source = { 1, 3, 4, 2, 8, 1 };
+            var result = source.Where(x => x < 4);
+            CollectionAssert.AreEqual(result, new int[]{1,3,2,1}); 
+        }
+
+        [Test]
+        public void Where_QueryExpressionSimpleFiltering()
+        {
+            int[] source = { 1, 3, 4, 2, 8, 1 };
+            var result = from x in source
+                where x < 4
+                select x;
+            CollectionAssert.AreEqual(result, new int[] { 1, 3, 2, 1 });
+        }
+    }
+}
---
+++ ReLINQ/ReLINQ.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReLINQ
+{
+    public static class ReLINQ
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="sequence"></param>
+        /// <param name="predicate"></param>
+        /// <returns><see cref="IEnumerable{T}"/></returns>
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> sequence, Func<TSource, bool> predicate)
+        {
+            if (predicate==null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            if (sequence==null)
+            {
+                throw  new ArgumentNullException(nameof(sequence));
+            }
+
+            return WhereImplementation(sequence, predicate);
+        }
+
+        /// <summary>
+        /// Where inner implementation
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="sequence"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        private static IEnumerable<TSource> WhereImplementation<TSource>(this IEnumerable<TSource> sequence, Func<TSource, bool> predicate)
+        {
+            foreach (TSource item in sequence)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Where Method which takes a predicate with an int parameter representing the index
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="sequence"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> sequence, Func<TSource,int, bool> predicate)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            if (sequence == null)
+            {
+                throw new ArgumentNullException(nameof(sequence));
+            }
+
+            return WhereImplementation(sequence, predicate);
+        }
+
+        /// <summary>
+        /// Inner implementation
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="sequence"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        private static IEnumerable<TSource> WhereImplementation<TSource>(this IEnumerable<TSource> sequence, Func<TSource,int, bool> predicate)
+        {
+            int index = 0;
+            foreach (TSource item in sequence)
+            {
+                if (predicate(item, index))
+                {
+                    yield return item;
+                }
+                index++;
+            }
+        }
+
+    }
+}
